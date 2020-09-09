154979044b1db0432771b60eb0a7fdb8e847266b
Manuel Lopez <w.mlc32@gmail.com>
2020-09-09 22:20:42 +0200

    Docs agregado

---
+++ docs/precache-manifest.28ff2ec433f6aa2a5500f0db4b43f2fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef7a36538ff5fe74bf45ee6d43f3dd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941ddb870dbb01b0fed0",
+    "url": "/static/css/main.7e6d90c1.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941ddb870dbb01b0fed0",
+    "url": "/static/js/main.db5c207f.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
